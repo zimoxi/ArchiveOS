230bbb2c57b4e5c07f04f44339dd1dc63831f930
zimoxi <zimoxi@users.noreply.github.com>
2026-06-29 00:41:40 +0800

    feat: apply UpdatePack0004

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,1 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import router from "./router";
-import App from "./App.vue";
-
-const app = createApp(App);
-app.use(createPinia());
-app.use(router);
-app.mount("#app");
+console.log("OpenFDAMS frontend running")
